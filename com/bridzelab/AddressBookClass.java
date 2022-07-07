8f121ef7d20c9787928e359a2deb648cd090a5e4
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-07 12:39:40 +0530

    Added : Editing exsting contacts

--- com/bridzelab/AddressBookClass.java
+++ com/bridzelab/AddressBookClass.java
@@ -11,6 +11,7 @@ public class AddressBookClass {
         System.out.println("Welcome to AddressBook program");
         AddressBookClass addressBookClass =new AddressBookClass();
         addressBookClass.addContact();
+        addressBookClass.editContacts();
     }
 
     public void addContact() {
@@ -35,4 +36,70 @@ public class AddressBookClass {
         System.out.println(contact);
         System.out.println("Contact added success fully");
     }
+    public void editContacts() {
+        System.out.println("Enter first or last name  to edit ");
+        String editName = scanner.next();
+        for (int i = 0; i < contact.size(); i++) {
+            if (contact.get(i).getFirstName().equals(editName) || contact.get(i).getLastName().equals(editName)) {
+                System.out.println("Select options \n1.first name \n2.last name\n3.address\n4.city\n5.state\n6.email\n7.zipcode\n8.phoneNum\n9.Exit ");
+                int edit = scanner.nextInt();
+                switch (edit) {
+                    case 1 -> {
+                        System.out.println("Enter first name ");
+                        String editFirstName = scanner.next();
+                        contact.get(i).setFirstName(editFirstName);
+                        System.out.println(editFirstName);
+                    }
+                    case 2 -> {
+                        System.out.println("Enter last name ");
+                        String editLastName = scanner.next();
+                        contact.get(i).setLastName(editLastName);
+                        System.out.println(editLastName);
+                    }
+                    case 3 -> {
+                        System.out.println("Enter Address ");
+                        String editAddress = scanner.next();
+                        contact.get(i).setAddress(editAddress);
+                        System.out.println(editAddress);
+                    }
+                    case 4 -> {
+                        System.out.println("Enter city ");
+                        String editCity = scanner.next();
+                        contact.get(i).setCity(editCity);
+                        System.out.println(editCity);
+                    }
+                    case 5 -> {
+                        System.out.println("Enter state ");
+                        String editState = scanner.next();
+                        contact.get(i).setState(editState);
+                        System.out.println(editState);
+                    }
+                    case 6 -> {
+                        System.out.println("Enter email ");
+                        String editEmail = scanner.next();
+                        contact.get(i).setEmail(editEmail);
+                        System.out.println(editEmail);
+                    }
+                    case 7 -> {
+                        System.out.println("Enter zipcode");
+                        String editZip = scanner.next();
+                        contact.get(i).setZip((editZip));
+                        System.out.println(editZip);
+                    }
+                    case 8 -> {
+                        System.out.println("Enter phone number ");
+                        String editPhoneNumber = scanner.next();
+                        contact.get(i).setPhoneNum((editPhoneNumber));
+                        System.out.println(editPhoneNumber);
+                    }
+                    default -> System.out.println("Exit the loop");
+                }
+                System.out.println("Contact edited successfully");
+                System.out.println(contact);
+            } else {
+                System.out.println("Contact not found ");
+            }
+        }
+    }
+
 }
